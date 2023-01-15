da060f4b8d0b4d85b205162af7ed100fec0b9309
MarcosSouzaa <marcossouza.git@gmail.com>
2023-01-14 21:47:49 -0300

    first commit com swagger adicionado e configurado

---
+++ src/main/java/com/marcosantoniosouza/bookstoremanager/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.marcosantoniosouza.bookstoremanager.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @ApiOperation(value = "Return an exemple Hello World")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sucess Method Return")
+    } )
+    @GetMapping
+    public String hello() {
+       return "Hello Book Store Manager";
+    }
+}
